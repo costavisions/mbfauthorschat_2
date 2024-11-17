32a8a8d7466baa6e71a6899a1330d9bc34025dbf
Christian Vivas <160914543+ChrisVivasAI@users.noreply.github.com>
2024-11-17 14:33:33 -0500

    Update next.config.js for domain pointing

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
 module.exports = withBundleAnalyzer({
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
   },
-})
+  basePath: '/authors-chatbot', // Add the basePath configuration here
+});
