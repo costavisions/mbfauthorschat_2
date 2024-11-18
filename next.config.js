db911aabb0538bbd605f265ed32b67b8d23bf707
Christian Vivas <160914543+ChrisVivasAI@users.noreply.github.com>
2024-11-18 13:16:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,4 +9,16 @@ module.exports = withBundleAnalyzer({
   basePath: '/authors-chatbot', // Ensure routes are prefixed with /authors-chatbot
   assetPrefix: 'https://stingray-app-kuz8f.ondigitalocean.app/authors-chatbot/', // Corrected to include the basePath
   crossOrigin: 'anonymous', // Add this for handling CORS issues
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, DELETE, PATCH, POST, PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
 });
